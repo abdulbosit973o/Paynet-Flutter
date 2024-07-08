be08fb92f71677c4b6fd427cde1c3461c2856e82
abdulbosit <abdulloh009730@gmail.com>
2024-07-08 11:46:01 +0500

    app package is done

---
+++ lib/features/paynet/presentation/pin_code_creator/pin_create_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PinCreateScreen extends StatefulWidget {
+  const PinCreateScreen({super.key});
+
+  @override
+  State<PinCreateScreen> createState() => _PinCreateScreenState();
+}
+
+class _PinCreateScreenState extends State<PinCreateScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
