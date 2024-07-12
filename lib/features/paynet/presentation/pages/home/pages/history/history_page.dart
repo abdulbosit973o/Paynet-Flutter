ef637c2a2ad581e75ba0cf81579bb6b6f7ef154f
Saidmurod Tursunaliyev <tursunaliyevsaidmurod@gmail.com>
2024-07-12 14:36:59 +0500

    history_page.dart

--- lib/features/paynet/presentation/pages/home/pages/history/history_page.dart
+++ lib/features/paynet/presentation/pages/home/pages/history/history_page.dart
@@ -1,11 +1,58 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'History App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HistoryPage(),
+    );
+  }
+}
+
 class HistoryPage extends StatelessWidget {
   const HistoryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            children:[
+              Text(
+                'Tarix',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'PaynetB',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28),
+              ),
+              Spacer(),
+              Icon(Icons.search,size: 24,)
+            ]
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+
+        ),
+      ),
     );
   }
 }
